0fd025426dad46b477515721b7eabe684f159690
Max Erfodimo <maxerfodimo@gmail.com>
2025-08-10 21:22:00 +0200

    fix: del listen

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Middleware
 app.use(helmet()); // Security headers
